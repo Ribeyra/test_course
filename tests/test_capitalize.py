f8c6b641c0da191fedce83dab37787c537be5541
ribeyra <ribeyra@gmail.com>
2024-01-23 15:33:29 +0300

    --

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -4,14 +4,14 @@ from test_course.capitalize import capitalize
 def test_capitalize():
     assert capitalize('hello') == 'Hello'
     assert capitalize('1ello') == '1ello'
+    print('\ntest_capitalize - pass')
 
 
 def test_capitalize_for_int():
     assert capitalize(12) == 12
+    print('\ntest_capitalize_for_int - pass')
 
 
 def test_capitalize_for_empty_str():
     assert capitalize('') == ''
-
-
-# print('pass')
+    print('\ntest_capitalize_for_empty_str - pass')
